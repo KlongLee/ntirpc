9f4d02cd60fad2a1211082d60431ca7dac1e99d6
Khalid Jamal Abdulnasser <khalid.abdulnasser1@gmail.com>
2021-11-03 16:38:07 +0100

    Revert "Merge branch 'GettingRidOfVarargs' of https://github.com/Safrout1/ntirpc into GettingRidOfVarargs"
    This reverts commit 178a7c85831c95ebec50222fe51d5d13d22fc963, reversing
    changes made to c800e7451acda717ca8f149bbab9a832a119f249.

---
+++ ntirpc/version.h
@@ -0,0 +1,15 @@
+/* version.h file expanded by Cmake for build */
+
+#ifndef VERSION_H
+#define VERSION_H
+
+#define NTIRPC_VERSION_MAJOR 3
+#define NTIRPC_VERSION_MINOR .2
+#define NTIRPC_VERSION_EXTRA 
+
+#define NTIRPC_VERSION "3.2"
+#define NTIRPC_VERSION_COMMENT "Full-duplex and bi-directional ONC RPC on TCP."
+#define _GIT_HEAD_COMMIT "be092f7e5ea7f17d3f17dc0ee5020243845c44b9"
+#define _GIT_DESCRIBE "V4-dev.72"
+
+#endif /* VERSION_H */
